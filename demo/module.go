ab08aa45a64ee3e4b1dd6bf41e2e9085bd2c3125
Mangesh Chandrashekhar Tanpure <mangesh.tanpure@quickheal.com>
2020-03-20 11:37:42 +0530

    1st commit

---
+++ demo/module.go
@@ -0,0 +1,23 @@
+package demo
+
+import (
+	"fmt"
+)
+
+type User struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+}
+
+func Userdata() User {
+	fmt.Println("hi there")
+	users := User{
+		ID:    1,
+		Name:  "mangesh",
+		Email: "mangesh@gmail.com",
+		Phone: "9970814741"}
+	return users
+
+}
